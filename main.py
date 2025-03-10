7a73c4d60edcfddf57521b2c04cb8e8b4683be2e
tnishanth94 <88890083+tnishanth94@users.noreply.github.com>
2025-03-10 11:28:22 +0530

    Updated main.py with latest imports and functions

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 import os
 from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from app.bm25_search import bm25_search
 
 app = FastAPI()
 app.add_middleware(
@@ -13,13 +16,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(router)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+DATA_FOLDER = os.path.join(os.path.dirname(__file__), "app", "data")
+
+@app.get("/pdf/{pdf_name}")
+async def get_pdf(pdf_name: str):
+    pdf_path = os.path.join(DATA_FOLDER, pdf_name)
+    print(f"Trying to serve PDF: {pdf_path}")
+    if not os.path.isfile(pdf_path):
+        raise HTTPException(status_code=404, detail="PDF not found")
+    return FileResponse(pdf_path, media_type="application/pdf")
+
+@app.get("/keyword_search/")
+async def keyword_search(query: str, top_k: int = 5):
+    return bm25_search(query, top_k)
 
 if not os.path.exists("uploads"):
     os.makedirs("uploads")
 
-if not os.path.exists("static/images"):
-    os.makedirs("static/images")
+if not os.path.exists("app/static/images"):
+    os.makedirs("app/static/images")
 
 if __name__ == "__main__":
     import uvicorn
